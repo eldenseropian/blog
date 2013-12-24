882e9c8146ba9e72f013754ba9fc0565bf6a17a0
Lily Seropian <lilyseropian@gmail.com>
2013-12-24 18:45:10 -0500

    Added missing semicolon

--- articleprovider-mongodb.js
+++ articleprovider-mongodb.js
@@ -1,4 +1,4 @@
-var Db = require('mongodb').Db
+var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
