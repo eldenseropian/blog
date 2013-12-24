6d21f76a09506f8f87bfd96e1808a1f4503f033a
Lily Seropian <seropian@mit.edu>
2013-12-24 13:19:34 -0500

    Ready to switch to linux
    Signed-off-by: Lily Seropian <lilyseropian@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
 var app = module.exports = express();
 
@@ -21,11 +21,11 @@ app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
-var articleProvider = new ArticleProvider();
+var articleProvider = new ArticleProvider('localhost', 27017);
 
-app.get('/', function(req, res){
-    articleProvider.findAll( function(error,docs){
-        res.render('index.jade', {title: 'Blog', articles: docs});
+app.get('/', function(request, response){
+    articleProvider.findAll(function(error, docs) {
+        response.render('index.jade', {title: 'Blog', articles: docs});
     });
 });
 
@@ -42,4 +42,23 @@ app.post('/blog/new', function(request, response) {
   });
 });
 
-app.listen(3000);
+app.get('/blog/:id', function(request, response) {
+	articleProvider.findById(request.params.id, function (error, article) {
+		response.render('blog_show.jade', {title: article.title, article: article});
+	});
+});
+
+app.post('/blog/addComment', function(request, response) {
+	articleProvider.addCommentToArticle(request.param('_id'), {
+		person: request.param('person'),
+		comment: request.param('comment'),
+		created_at: new Date()
+	}, function(error, docs) {
+		response.redirect('/blog/' + req.param('_id'));
+	});
+});
+
+var listening_app = app.listen(3000);
+
+console.log('Express server listening on port %d in %s mode',
+    listening_app.address().port, app.settings.env);
